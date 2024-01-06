4a33ac60dcd73bc37ad77c2ca4dfed7bcc5e9ad1
James Dixon <notjamesdixon@gmail.com>
2024-01-06 03:18:07 -0500

    parse flag for port and rename file

--- static-http-server.go
+++ static-http-server.go
@@ -7,14 +7,16 @@ import (
 	"net/http"
 )
 
-var port = flag.String("port", ":8090", "port for server to listen on")
+var port = flag.String("port", "8090", "port for server to listen on")
 
 func main() {
+	flag.Parse()
+
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	log.Print(fmt.Sprintf("Listening on port :%s", *port))
-	err := http.ListenAndServe(*port, nil)
+	err := http.ListenAndServe(":" + *port, nil)
 	if err != nil {
 		log.Fatal()
 	}
